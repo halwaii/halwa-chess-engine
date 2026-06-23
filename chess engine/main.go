5f7032363d54ba183419dac86138de4e0b073c10
halwaii <dacron1005@gmail.com>
2026-06-23 12:42:51 +0530

    initialized chess board setup

---
+++ chess engine/main.go
@@ -0,0 +1,5 @@
+package main
+import "fmt"
+func main(){
+	fmt.Println("i am going to make chess engine :P")
+}
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+import "fmt"
+func main(){
+	fmt.Println("welcome to halwa chess engine :P\n")
+	board := [8][8]string{
+		{"r","n","b","q","k","b","n","r"},
+		{"p","p","p","p","p","p","p","p"},
+		{".",".",".",".",".",".",".","."},
+		{".",".",".",".",".",".",".","."},
+		{".",".",".",".",".",".",".","."},
+		{".",".",".",".",".",".",".","."},
+		{"P","P","P","P","P","P","P","P"},
+		{"R","N","B","Q","K","B","K","R"},
+	}
+	for i:=0;i<8;i++{
+		for j:=0;j<8;j++{
+			fmt.Printf("%v",board[i][j])
+		}
+		fmt.Println();
+	}
+}
